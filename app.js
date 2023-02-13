1190696b20f2bbab46247bea09eb70828d858a5b
Marcos Casañas <mcasaas@st.local>
2023-02-13 14:07:19 -0300

    nuevos cambios

--- app.js
+++ app.js
@@ -4,10 +4,14 @@
 //Variables
 const tipoSal = document.querySelector('#tipos')
 const cantidad = parseInt(document.querySelector('#cantidad').value)
-
 const btnAgregar = document.querySelector('#btnAgregar')
 const infoProd = document.querySelector('#infoProd')
 const formAdd= document.querySelector('#formAdd')
+
+const userHtml = document.querySelector("#imprimir")
+const btnIngresar = document.querySelector("#btnIngreso")
+
+
 let producciones = []
 mensaje()
 detectarTipo()
@@ -16,13 +20,30 @@ detectarCantidad()
 cargarEventListeners()
 
 function cargarEventListeners(){
-    
+ 
 formAdd.addEventListener('click' , agregarProd)
 infoProd.addEventListener('click', eliminarProd)
+document.addEventListener("DOMContentLoaded", () => {
+  let datosLS = JSON.parse(localStorage.getItem("tareas")) || [];
+  task = datosLS;
+  
+  produccionHTML()
+})
 }
 
-
+let datosLS = JSON.parse(localStorage.getItem("tareas")) || [];
+producciones = datosLS;
 //FUNCIONES
+
+
+  
+
+
+var myModal = new bootstrap.Modal(document.getElementById("staticBackdrop"), {});
+document.onreadystatechange = function () {
+  myModal.show();
+};
+
 
 function agregarProd(e){
     
@@ -136,7 +157,18 @@ function produccionHTML(){
         const row = document.createElement('tr')
         let today = new Date()
         let now = today.toLocaleString()
+
+        let palletE = Number.parseInt(produccion.cantidad/56)
+        let palletP =produccion.cantidad +"bolsas"
+        if(produccion.cantidad===56){
+        palletE
+        console.log("si")
+        }else{
+          palletP
+          console.log("no")
+        }
         row.innerHTML=`
+
 
         <div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
@@ -146,8 +178,9 @@ function produccionHTML(){
     <div class="col-md-8">
       <div class="card-body">
       <p class="card-text 10px"><small class="text-muted">${now}</small></p>
-        <h5 class="card-title h3">${produccion.tipo} <a href="#" class="borrar btn btn-outline-danger"" data-id="${produccion.id}" >X <a/></h5>
-        <p class="card-text"><small class="h6">Cantidad Total : ${produccion.cantidad} </small></p>
+        <h5 class="card-title h4">${produccion.tipo} <a href="#" class="borrar btn btn-outline-danger"" data-id="${produccion.id}" >X <a/></h5>
+        <p class="card-text"><small class="text-muted">Cantidad Total bolsas : ${produccion.cantidad} </small></p>
+        <p class="card-text"><small class="text-muted">Cantidad Pallets : ${palletE} </small></p>
         
         
       </div>
@@ -164,7 +197,13 @@ function produccionHTML(){
 infoProd.appendChild(row)
 
 
+
+    
+
     })
+
+    //persistir los datos con localStorage
+    localStorage.setItem("tareas", JSON.stringify(producciones))
 }
 
 
@@ -211,3 +250,40 @@ function detectarCantidad(){
 }
 
 
+
+
+
+
+
+
+
+
+
+btnIngresar.addEventListener("click" , users)
+  let user = document.querySelector("#persona")
+
+function users (){
+  
+  let horario = document.querySelector("#horario")
+  userHtml.innerHTML = `
+   Operario: ${user.value} - Turno: ${horario.value}
+  
+  
+  `
+  
+  myModal.hide()
+
+  
+console.log(user.value)
+
+
+}
+
+function detectarModal(){
+  while (userHtml) {
+    myModal.show()
+  }
+  
+  
+  
+}
